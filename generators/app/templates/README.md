a458840706d9b468e03bfb1c1188b41a9fc28f72
hugojing <toloric@163.com>
2017-03-23 18:38:34 +0800

    [ADD] config file split, dev server added.

--- generators/app/templates/README.md
+++ generators/app/templates/README.md
@@ -1,1 +1,7 @@
-# MY PROJECT NAME
+# template-webpack-babel
+
+2017 Basic Web dev template for newbee, and readers of hugojing/web-lessons.
+
+## Usage
+
+click → https://github.com/hugojing/generator-webpack-babel
--- generators/app/templates/src/main.js
+++ generators/app/templates/src/main.js
@@ -5,4 +5,13 @@ const logoSrc = require('@/assets/images/yeoman-logo.png')
 
 const logo = document.createElement('img')
 logo.setAttribute('src', logoSrc)
-document.getElementById('app').appendChild(logo)
+// document.getElementById('app').appendChild(logo)
+
+function append() {
+  require.ensure(['jquery'], require => {
+    const $ = require('jquery')
+    $('#app').append(logo)
+  })
+}
+
+setTimeout(append, 0)
